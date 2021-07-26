0511ed0f55146081757ff440af26e801a83577be
Muhammad Nasr Ahmad <79932064+ahmad047@users.noreply.github.com>
2021-07-26 23:00:02 +0100

    ex4 - refactored to create a private function swap

--- src/04-sort-array/index.js
+++ src/04-sort-array/index.js
@@ -17,12 +17,16 @@ module.exports = function sortArray(arr) {
     for (let i = 0; i < arr.length; i++) {
       for (let j = 0; j < arr.length - 1; j++) {
         if (arr[j] > arr[j + 1]) {
-          let x = arr[j];
-          arr[j] = arr[j + 1];
-          arr[j + 1] = x;
+          _swap(arr, j);
         }
       }
     }
     return arr;
   }
+};
+
+const _swap = (arr, jIndex) => {
+  let temp = arr[jIndex];
+  arr[jIndex] = arr[jIndex + 1];
+  arr[jIndex + 1] = temp;
 };
