5b29ab6921591cac88129c0ff1a65326f13d87d3
Muhammad Nasr Ahmad <79932064+ahmad047@users.noreply.github.com>
2021-07-26 22:55:34 +0100

    ex4 - used bubble sort to pass test 2 and create a rough solution

--- src/04-sort-array/index.js
+++ src/04-sort-array/index.js
@@ -11,7 +11,18 @@
 // Explain:
 
 module.exports = function sortArray(arr) {
-  if(arr.length == 0) {
-    return arr
+  if (arr.length == 0) {
+    return arr;
+  } else {
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < arr.length - 1; j++) {
+        if (arr[j] > arr[j + 1]) {
+          let x = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = x;
+        }
+      }
+    }
+    return arr;
   }
 };
