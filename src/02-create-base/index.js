2ee5302fc08e44508203680d52cf04ec96f9c7b4
Muhammad Nasr Ahmad <79932064+ahmad047@users.noreply.github.com>
2021-07-26 22:28:32 +0100

    ex2 - added big o

--- src/02-create-base/index.js
+++ src/02-create-base/index.js
@@ -1,7 +1,11 @@
 // You can assume the input is an integer
 
-// Average and worst case time complexity (Big-O) of my function is:
-// Explain:
+// Average and worst case time complexity (Big-O) of my function is: 
+  // it is O(1)
+
+// Explain: 
+  // regardless of the second integer, the same operation is performed
+  // which is a simple addition
 
 module.exports = function createBase(integer) {
   return functionForAddition = secondInteger => {
